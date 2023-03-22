775ac4bc194c2951068421d4062ac247d53d5c14
lhk3337 <lhk3337@naver.com>
2023-03-22 15:03:37 +0900

    [FEAT] item list api data component 추가

--- src/javascript/components/item/itemList.js
+++ src/javascript/components/item/itemList.js
@@ -1,12 +1,11 @@
-function ItemList({ $target, data }) {
+function ItemList({ $main, data }) {
   const render = async () => {
     const datas = await data;
-    $target.innerHTML = `
-        <main>
-          <div class="product_container">
-            ${datas
-              .map((value, i) => {
-                return `
+    $main.innerHTML = `
+          <ul class="product_container">
+              ${datas
+                .map((value, i) => {
+                  return `
                 <li class="product_list" key=${i}>
                   <img class="product_img" src=http://211.243.164.209:5000/${value.thumbnailImg} alt="thumbnailImg" />
                   <div class="product_content">
@@ -17,16 +16,28 @@ function ItemList({ $target, data }) {
                       <button class="heart_btn" />
                     </div>
                     <div class="product_value">
-                      <span class="price">${value.price}</span>원
+                      ${
+                        value.discountRate
+                          ? `<span class="price">${Math.floor(
+                              value.price * ((100 - value.discountRate) / 100)
+                            ).toLocaleString("ko-KR")}</span>원`
+                          : `<span class="price">${value.price.toLocaleString("ko-KR")}</span>원`
+                      }
+                      ${
+                        value.discountRate
+                          ? `<span class="origin_price">${value.price.toLocaleString("ko-KR")}원</span>`
+                          : ""
+                      }
                       <span class="discount">${value.discountRate > 0 ? `${value.discountRate}%` : ""}</span>
                     </div>
                   </div>
                 </li>
                 `;
-              })
-              .join("")}
-          </div>
-        </main>`;
+                })
+                .join("")}
+          </ul>
+          <beutton class="cart__btn"><img src="/src/assets/cart-btn.svg"></beutton>
+        `;
   };
   return render();
 }
