a63cc0a305df1a235b8de37b7a4e09ccef58d6d2
lhk3337 <lhk3337@naver.com>
2023-03-23 19:35:24 +0900

    item list 클릭 이벤트 및 like btn 설정하기

--- src/javascript/components/item/itemList.js
+++ src/javascript/components/item/itemList.js
@@ -1,19 +1,22 @@
-function ItemList({ $main, data }) {
-  const render = async () => {
-    const datas = await data;
+import ItemDetail from "./itemDetail.js";
+import { getItem, setItem, removeItem } from "../../lib/storage.js";
+import { routeChange } from "../../lib/router.js";
+export default function ItemList({ $main, apiData }) {
+  this.render = async () => {
+    const productData = await apiData;
     $main.innerHTML = `
           <ul class="product_container">
-              ${datas
+              ${productData
                 .map((value, i) => {
                   return `
-                <li class="product_list" key=${i}>
-                  <img class="product_img" src=http://211.243.164.209:5000/${value.thumbnailImg} alt="thumbnailImg" />
+                <li class="product_list" data-key=${value.id}>
+                <img class="product_img" src=http://211.243.164.209:5000/${value.thumbnailImg} alt="thumbnailImg" />
                   <div class="product_content">
                     <div class="product_title">
                       <span>${
                         value.productName.length < 30 ? value.productName : value.productName.substring(0, 25)
                       }</span>
-                      <button class="heart_btn" />
+                      ${getItem(value.id, "") ? `<button class="heart_btn" />` : `<button class="heart_btn_cancel" />`}
                     </div>
                     <div class="product_value">
                       ${
@@ -35,10 +38,30 @@ function ItemList({ $main, data }) {
                 `;
                 })
                 .join("")}
-          </ul>
-          <beutton class="cart__btn"><img src="/src/assets/cart-btn.svg"></beutton>
-        `;
+              </ul>
+              <button class="cart_btn"><img src="/src/assets/cart-btn.svg"></button>
+              `;
+
+    // evnet click handling
+    Array.from(document.querySelectorAll(".product_list")).map((v) =>
+      v.addEventListener("click", (e) => {
+        if (e.target.className === "heart_btn_cancel") {
+          setItem(v.dataset.key, true);
+          this.render();
+        } else if (e.target.className === "heart_btn") {
+          removeItem(v.dataset.key);
+          this.render();
+        } else {
+          if (v.dataset.key) {
+            new ItemDetail({ $main, id: v.dataset.key }).render();
+          }
+        }
+      })
+    );
+
+    document.querySelector(".cart_btn").addEventListener("click", () => {
+      routeChange("/cart");
+    });
   };
-  return render();
+  // 해당 아이템 클릭 시 상세 아이템 api 데이터 출력 및 하트 버튼에 대한 핸들링
 }
-export default ItemList;
