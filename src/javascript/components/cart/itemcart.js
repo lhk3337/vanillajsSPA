84926f1bd8b278b1f5c62d4130f166f3bb5adbca
lhk3337 <lhk3337@naver.com>
2023-03-23 19:32:54 +0900

    [FEAT] cart page 생성하기

--- src/javascript/components/cart/itemcart.js
+++ src/javascript/components/cart/itemcart.js
@@ -1,7 +1,18 @@
-function ItemCart({ $main, data }) {
-  const render = async () => {
-    $main.innerHTML = `<div><h1>Hello World</h1></div>`;
+import { routeChange } from "../../lib/router.js";
+
+function ItemCart({ $main }) {
+  this.render = async () => {
+    $main.innerHTML = `
+    <div>
+      <h1>Hello World</h1>
+    </div>
+    <button class="home_btn">
+        <img src="/src/assets/icon-home.svg">
+    </button>
+    `;
+    document.querySelector(".home_btn").addEventListener("click", () => {
+      routeChange("/");
+    });
   };
-  render();
 }
 export default ItemCart;
