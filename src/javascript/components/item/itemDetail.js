ddcf1333ba46d5f28649fa2fcbdf26ca07f728f7
lhk3337 <lhk3337@naver.com>
2023-05-02 20:30:08 +0900

    [FEAT] option 버튼 클릭 시 option box 나타내기 및  없애기 설정하기

--- src/javascript/components/item/itemDetail.js
+++ src/javascript/components/item/itemDetail.js
@@ -3,9 +3,11 @@ import { getItem, setItem, removeItem } from "../../lib/storage.js";
 function ItemDetail({ $target, id, listRender }) {
   this.state = { apiAddr: null, apiData: null };
   this.likeState = { liked: Boolean(getItem(id, "")) };
-  this.countState = {
-    value: 1,
+  this.noOptionState = {
+    countValue: 1,
     totalvalue: 0,
+  };
+  this.optionState = {
     optionValue: [],
   };
 
@@ -25,15 +27,19 @@ function ItemDetail({ $target, id, listRender }) {
     const apiAddr = (await api()).API_ADDR;
     const apiData = await (await api()).fetchProduct(id);
     this.setState({ ...this.state, apiAddr, apiData });
-    this.setcountState({ ...this.countState, totalvalue: apiData.price });
+    this.setOptionState({ id: apiData.id, ...this.optionState });
+    this.setNoOptionState({ ...this.noOptionState, id: apiData.id, totalvalue: apiData.price });
   };
-  this.setcountState = (nextState) => {
-    this.countState = nextState;
-    if (this.noOptionTotalCount()) {
-      this.noOptionTotalCount();
-    } else {
-      this.optionTotalCount();
-    }
+
+  this.setNoOptionState = (nextState) => {
+    this.noOptionState = nextState;
+    this.noOptionTotalCount();
+  };
+
+  this.setOptionState = (nextState) => {
+    this.optionState = nextState;
+    this.OptionCount();
+    this.optionTotalCount();
   };
 
   this.data();
@@ -128,13 +134,12 @@ function ItemDetail({ $target, id, listRender }) {
                 <img src="/src/assets/icon-shopping-cart.svg" alt="addCartBtn" />
               </button>
             </div>
-          
         `;
-            this.Count();
+            this.noOptionCount();
             this.noOptionTotalCount();
           } else {
             $buyInput.innerHTML = `
-            <div class="count_input_container"></div>
+            <div class="option_input_container"></div>
             <div class="total_price_container"></div>
             <div class="product_stock_btns">
               <button class="submit_btn">바로 구매</button>
@@ -143,6 +148,7 @@ function ItemDetail({ $target, id, listRender }) {
               </button>
             </div>
         `;
+            this.selectedOption();
             this.OptionCount();
             this.optionTotalCount();
           }
@@ -167,62 +173,94 @@ function ItemDetail({ $target, id, listRender }) {
       $option__container.className = "option__container";
 
       // option이 없을 경우 Count 컴포넌트
-      this.Count = () => {
+      this.noOptionCount = () => {
         document.querySelector(".count_input_container").appendChild($counter);
         $counter.innerHTML = `
-        <div class="number_count_input_container">
-          <button class="minBtn"><img src="/src/assets/minus-icon-bg-white.svg" alt="minusCount" /></button>
-          <input class="numberCount" type="number" value="1" min="1" max=${apiData?.stockCount} />
-          <button class="plusBtn"><img src="/src/assets/plus-icon-bg-white.svg" alt="plusCount" /></button>
-        </div>`;
-      };
-
-      // 옵션 없는 총 상품 금액 표시 컴포넌트
-      this.noOptionTotalCount = () => {
-        if (document.querySelector(".numberCount")) {
-          document.querySelector(".total_price_container").innerHTML = `
-          <h1>총 상품 금액</h1>
-          <div class="total_price_info">
-          <span class="count_text">총 수량 <span class="count_number">${this.countState.value}</span>개</span>
-          <span class="total_price_count">${this.countState.totalvalue.toLocaleString("ko-KR")}</span>원
-          </div>
-          `;
-        }
-      };
-
-      // option Count 컴포넌트
-      this.OptionCount = () => {
-        document.querySelector(".count_input_container").appendChild($option__container);
-        $option__container.innerHTML = `
-        <div class="select-box">
-          <button class="toggle-btn">옵션을 선택하세요</button>
-          <ul class="select-box-option">
-            ${apiData.option
-              .map((v) => {
-                if (v.additionalFee > 0) {
-                  return `<li><button class="option-btn" data-option-id=${v.id}>${v.optionName}(+${v.additionalFee}원)</button></li>`;
-                } else {
-                  return `<li><button class="option-btn" data-option-id=${v.id}>${v.optionName}</button></li>`;
-                }
-              })
-              .join("")}
-          </ul>
           <div class="number_count_input_container">
             <button class="minBtn"><img src="/src/assets/minus-icon-bg-white.svg" alt="minusCount" /></button>
             <input class="numberCount" type="number" value="1" min="1" max=${apiData?.stockCount} />
             <button class="plusBtn"><img src="/src/assets/plus-icon-bg-white.svg" alt="plusCount" /></button>
-          </div>
-        </div>
-        `;
+          </div>`;
       };
-      this.optionTotalCount = () => {
+
+      // 옵션 없는 총 상품 금액 표시 컴포넌트
+      this.noOptionTotalCount = () => {
         document.querySelector(".total_price_container").innerHTML = `
           <h1>총 상품 금액</h1>
           <div class="total_price_info">
-          <span class="count_text">총 수량 <span class="count_number">${this.countState.value}</span>개</span>
-          <span class="total_price_count">${this.countState.totalvalue.toLocaleString("ko-KR")}</span>원
+          <span class="count_text">총 수량 <span class="count_number">${this.noOptionState.countValue}</span>개</span>
+          <span class="total_price_count">${this.noOptionState.totalvalue.toLocaleString("ko-KR")}</span>원
           </div>
           `;
+      };
+
+      // option Count 컴포넌트
+      this.selectedOption = () => {
+        if (document.querySelector(".option_input_container")) {
+          document.querySelector(".option_input_container").appendChild($option__container);
+          $option__container.innerHTML = `
+          <div class="select-box">
+          <button class="toggle-btn">옵션을 선택하세요</button>
+          <ul class="select-box-option">
+          ${apiData.option
+            .map((v) => {
+              if (v.additionalFee > 0) {
+                return `<li><button class="option-btn" data-option-id=${v.id}>${v.optionName}(+${v.additionalFee}원)</button></li>`;
+              } else {
+                return `<li><button class="option-btn" data-option-id=${v.id}>${v.optionName}</button></li>`;
+              }
+            })
+            .join("")}
+            </ul>
+            </div>
+            `;
+        }
+      };
+
+      const $selectedContainer = document.createElement("div");
+      $selectedContainer.className = "selected_container";
+
+      // 옵션 선택시 정보 및 카운트 input 표시되는 컴포넌트
+      this.OptionCount = () => {
+        if (document.querySelector(".option_input_container")) {
+          document.querySelector(".option_input_container").appendChild($selectedContainer);
+          if (this.optionState.optionValue) {
+            $selectedContainer.innerHTML = `
+          ${this.optionState.optionValue
+            .map((v) => {
+              return `
+              <div class="selected_option">
+                <h1>${v.optionName}</h1>
+                <button class="closeOption" data-selected-id="${v.id}">
+                  <img src="/src/assets/icon-delete.svg" alt="closeOptionButton" /> 
+                </button>
+                <div class="option_info">
+                  <div class="number_count_input_container">
+                    <button class="minBtn"><img src="/src/assets/minus-icon-bg-white.svg" alt="minusCount" /></button>
+                    <input class="numberCount" type="number" value="1" min="1" max=${apiData?.stockCount} />
+                    <button class="plusBtn"><img src="/src/assets/plus-icon-bg-white.svg" alt="plusCount" /></button>
+                  </div>
+                  <span>${v.price.toLocaleString("ko-KR")}원</span>
+                </div>
+              </div>
+            `;
+            })
+            .join("")}
+        `;
+          }
+        }
+      };
+
+      this.optionTotalCount = () => {
+        if (document.querySelector(".numberCount")) {
+          document.querySelector(".total_price_container").innerHTML = `
+          <h1>총 상품 금액</h1>
+          <div class="total_price_info">
+          <span class="count_text">총 수량 <span class="count_number">${this.noOptionState.countValue}</span>개</span>
+          <span class="total_price_count">${this.noOptionState.totalvalue.toLocaleString("ko-KR")}</span>원
+          </div>
+          `;
+        }
       };
 
       // 좋아요 버튼 컴포넌트
@@ -285,9 +323,33 @@ function ItemDetail({ $target, id, listRender }) {
       // 모달창에서 모달창 의외에 클릭할때 모달창 끄는 이벤트
 
       // option이 없는 경우 수량 카운트 input number handler
-      const $minBtn = document.querySelector(".minBtn");
-      const $plusBtn = document.querySelector(".plusBtn");
+      const $number_count_input_container = document.querySelector(".buy_input");
+
       const $numberCount = document.querySelector(".numberCount");
+      $number_count_input_container.addEventListener("click", (event) => {
+        const target = event.target;
+
+        const $minBtn = target.closest(".minBtn");
+        const $plusBtn = target.closest(".plusBtn");
+
+        if ($minBtn) {
+          let value = $numberCount.value;
+          value--;
+          if (value > 0) {
+            $numberCount.value = value;
+            this.setNoOptionState({ countValue: value, totalvalue: value * apiData.price });
+          }
+        }
+        if ($plusBtn) {
+          let value = $numberCount.value;
+          if (value < apiData?.stockCount) {
+            value++;
+          }
+          $numberCount.value = value;
+          this.setNoOptionState({ countValue: value, totalvalue: value * apiData.price });
+        }
+      });
+
       if ($numberCount) {
         $numberCount.onkeydown = (e) => {
           // 숫자, 백스페이스, 위아래 화살표 키 설정하기 (양수만 입력 가능)
@@ -303,30 +365,13 @@ function ItemDetail({ $target, id, listRender }) {
             return false;
           }
         };
+
         $numberCount.addEventListener("input", (e) => {
           const lengths = apiData?.stockCount;
           if (e.target.value > lengths) {
             e.target.value = lengths;
           }
-          this.setcountState({ value: e.target.value, totalvalue: e.target.value * apiData.price });
-        });
-        $minBtn.addEventListener("click", (e) => {
-          // - 버튼 클릭 시 input value -1씩 감소
-          let value = $numberCount.value;
-          value--;
-          if (value > 0) {
-            $numberCount.value = value;
-            this.setcountState({ value: value, totalvalue: value * apiData.price });
-          }
-        });
-        $plusBtn.addEventListener("click", (e) => {
-          // + 버튼 클릭 시 input value +1씩 증가
-          let value = $numberCount.value;
-          if (value < apiData?.stockCount) {
-            value++;
-          }
-          $numberCount.value = value;
-          this.setcountState({ value: value, totalvalue: value * apiData.price });
+          this.setNoOptionState({ countValue: e.target.value, totalvalue: e.target.value * apiData.price });
         });
       }
 
@@ -346,9 +391,10 @@ function ItemDetail({ $target, id, listRender }) {
         });
       }
 
-      // 옵션 버튼을 누른 후 옵션 박스 클릭 시, 수량 및 가겨 표시 박스 표시하기
+      // 옵션 선택 버튼과 해당 옵션 리스트 클릭 시, 수량 및 가격 표시하는 옵션 박스 표시하기
 
       const $optionSelectBtn = document.querySelectorAll(".option-btn");
+
       if ($optionSelectBtn) {
         $optionSelectBtn.forEach((element) => {
           const {
@@ -356,17 +402,49 @@ function ItemDetail({ $target, id, listRender }) {
           } = element;
           element.addEventListener("click", () => {
             const optionClickItem = apiData.option.find((v) => v.id === +optionId);
-            const selectedItem = this.countState.optionValue.find((v) => v.id === +optionId);
+            const selectedItem = this.optionState.optionValue.find((v) => v.id === +optionId);
+            const { id, optionName, additionalFee } = optionClickItem;
 
             if (optionClickItem && !selectedItem) {
-              this.setcountState({
-                ...this.countState,
-                optionValue: [...this.countState.optionValue, apiData.option.find((v) => v.id === +optionId)],
+              this.setOptionState({
+                ...this.optionState,
+                optionValue: [
+                  ...this.optionState.optionValue,
+                  {
+                    id,
+                    optionName,
+                    additionalFee,
+                    price: apiData.price + additionalFee,
+                  },
+                ],
               });
             }
           });
         });
       }
+
+      // 동적으로 생성된 요소에 이벤트 설정하기, 해당 옵션 박스의 X버튼 클릭 시 옵션 박스 삭제
+      $selectedContainer.addEventListener("click", (event) => {
+        // 정적으로 설정한 부모 요소 설정
+        const target = event.target;
+        const closeOptionButtonElement = target.closest(".closeOption");
+        if (closeOptionButtonElement) {
+          const {
+            dataset: { selectedId },
+          } = closeOptionButtonElement;
+          this.setOptionState({
+            ...this.optionState,
+            optionValue: this.optionState.optionValue.filter((v) => v.id !== +selectedId),
+          });
+        }
+      });
+      const $submitBtn = document.querySelector(".submit_btn");
+      $submitBtn.addEventListener("click", () => {
+        console.log(this.noOptionState);
+        const cartData = getItem("product_carts", []);
+
+        setItem("product_carts", cartData.concat(apiData.option.length > 0 ? this.optionState : this.noOptionState));
+      });
     }
   };
   // 반복으로 이벤트를 발생 시킬때 this.render내에 addEventListener가 있으면 느려져서 최상단에 위치 시킴
